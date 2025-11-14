637a36fd0ff55a1429fbe06ba2001db92d52207a
Joshua Ibrom <joshuaibrom@gmail.com>
2025-11-13 21:34:51 -0600

    refactor: redo how pages are defined
    Fix page definition to allow for there to be other page types. I think
    this new layout should work better and should afford a better DX.

--- src/content.zig
+++ src/content.zig
@@ -9,13 +9,20 @@ pub const Markup = union(MarkupTag) {
     html: []const u8,
 };
 
-pub const Post = struct {
+pub const PageTypeTag = enum {
+    home,
+    post,
+};
+
+pub const PageType = union(PageTypeTag) {
+    home: Home,
+    post: Post,
+};
+
+pub const Page = struct {
     const Self = @This();
 
-    title: struct { short: []const u8, long: []const u8 },
-    description: []const u8,
-    date: []const u8,
-    is_published: bool,
+    page_type: PageType,
     markup: Markup,
 
     pub fn get_markup(self: Self, allocator: std.mem.Allocator) ![]const u8 {
@@ -28,4 +35,18 @@ pub const Post = struct {
             .html => |htmls| htmls,
         };
     }
+};
+
+pub const Post = struct {
+    title: struct { short: []const u8, long: []const u8 },
+    description: []const u8,
+    post_path: []const u8,
+    date: []const u8,
+    is_published: bool,
+};
+
+pub const Home = struct {
+    title: struct { short: []const u8, long: []const u8 },
+    description: []const u8,
+    date: []const u8,
 };
--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,64 @@
 const std = @import("std");
 const content = @import("./content.zig");
 
-const posts: [2]content.Post = [_]content.Post{
-    content.Post{
-        .title = .{ .long = "Some long title", .short = "A title" },
-        .description = "Desc",
-        .date = "11-12-25",
-        .is_published = true,
-        .markup = .{ .file = "content/foo.html" },
+const pages: [3]content.Page = [_]content.Page{
+    content.Page{
+        .page_type = .{
+            .home = .{
+                .title = .{
+                    .long = "Blog Generator Example",
+                    .short = "Blog Example",
+                },
+                .date = "13 November 2025",
+                .description = "An example site",
+            },
+        },
+        .markup = .{ .html = "<h1>Blog site.</h1>" },
     },
-    content.Post{
-        .title = .{ .long = "Another title", .short = "Another title" },
-        .description = "Desc",
-        .date = "11-12-25",
-        .is_published = false,
-        .markup = .{ .html = "<em>Some content</em>" },
+    content.Page{
+        .page_type = .{
+            .post = .{
+                .title = .{
+                    .long = "Blog Generator Example",
+                    .short = "Blog Example",
+                },
+                .date = "13 November 2025",
+                .description = "An example site",
+                .is_published = true,
+                .post_path = "foo-bar",
+            },
+        },
+        .markup = .{ .file = "./content/foo.html" },
+    },
+    content.Page{
+        .page_type = .{
+            .post = .{
+                .title = .{
+                    .long = "Blog Generator Example",
+                    .short = "Blog Example",
+                },
+                .date = "13 November 2025",
+                .description = "An example site",
+                .is_published = false,
+                .post_path = "biz-baz",
+            },
+        },
+        .markup = .{ .html = "<em>Blog site post.</em>" },
     },
 };
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
-    for (posts) |p| {
-        std.debug.print("{s}\n", .{try p.get_markup(allocator)});
+    for (pages) |page| {
+        switch (page.page_type) {
+            .home => |pg| {
+                std.debug.print("Home Page: {s}\n", .{pg.title.long});
+            },
+            .post => |pg| {
+                std.debug.print("Post Page: {s} ({s})\n", .{ pg.title.long, pg.description });
+            },
+        }
+        const html = try page.get_markup(allocator);
+        std.debug.print("{s}\n", .{html});
     }
 }
